8f5848087e56137e3476cba230163a01e8f37176
sandra-glez <sandraglez0925@gmail.com>
2024-09-28 23:04:05 +0200

    coño

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -134,7 +134,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp/static'),  # Ajusta esto a la ubicación correcta
+]
 
+# Para producción, necesitas definir dónde se recogerán los archivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
