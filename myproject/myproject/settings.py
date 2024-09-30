4c6d8641d236ee08f6be65cd29aafaa624de2e51
gonzalo-ceb <gonzalo.celaya.ceb@immune.institute>
2024-09-30 11:56:04 +0200

    lol

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -121,7 +121,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': config('DB_NAME'),
     }
 }
 
