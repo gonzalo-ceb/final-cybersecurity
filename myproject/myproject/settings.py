fe8574a7f57d68b1e30190099b2d5244fce998df
sandra-glez <sandraglez0925@gmail.com>
2024-10-02 10:48:13 +0200

    staticcoño2

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -138,7 +138,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
+# Directorios adicionales donde Django buscará archivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myapp/static'),  # Ruta a tu carpeta de archivos estáticos dentro de la app
+]
+
+# Directorio donde se recopilarán todos los archivos estáticos cuando ejecutes collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
 
